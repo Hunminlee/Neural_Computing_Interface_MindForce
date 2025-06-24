41ec5fa16349b755bba6379a090e60949876b96b
hml76 <hml763@naver.com>
2025-06-24 13:56:13 -0500

    6/24

---
+++ V1_modular/config.py
@@ -0,0 +1,13 @@
+default_path = "C:/Users/hml76/OneDrive/문서/MATLAB/data/"
+classes = ['1/','2/','3/','4/','5/']
+
+Info = ['Stand (5/27)', 'Stand (6/18)', 'Stand (6/20)', 'Sit_chair (6/20)', 'Sit_chair_leg_crossed (6/20)', 'Sit_floor (6/20)',
+        'Stand (6/20-v2)', 'Sit_chair (6/20-v2)', 'Sit_chair_leg_crossed (6/20-v2)', 'Sit_floor (6/20-v2)',
+        'Stand (6/23)', 'Sit_chair (6/23)', 'Sit_chair_leg_crossed (6/23)', 'Sit_floor (6/23)',
+        'Stand (6/24)', 'Sit_chair (6/24)', 'Sit_chair_leg_crossed (6/24)', 'Sit_floor (6/24)']
+
+
+dataset = ["Exp_2025-05-27/E8331D05289A/", "Exp_2025-06-18/E9AD0E7DCC2B/", "Exp_2025-06-20-v1/E9AD0E7DCC2B/", "Exp_2025-06-20-v2/E9AD0E7DCC2B/", "Exp_2025-06-20-v3/E9AD0E7DCC2B/",
+           "Exp_2025-06-20-v4/E9AD0E7DCC2B/", "Exp_2025-06-20-v5/E9AD0E7DCC2B/", "Exp_2025-06-20-v6/E9AD0E7DCC2B/", "Exp_2025-06-20-v7/E9AD0E7DCC2B/", "Exp_2025-06-20-v8/E9AD0E7DCC2B/",
+           "Exp_2025-06-23-v1/E9AD0E7DCC2B/", "Exp_2025-06-23-v2/E9AD0E7DCC2B/", "Exp_2025-06-23-v3/E9AD0E7DCC2B/", "Exp_2025-06-23-v4/E9AD0E7DCC2B/",
+           "Exp_2025-06-24-v4/E9AD0E7DCC2B/", "Exp_2025-06-24-v4/E9AD0E7DCC2B/", "Exp_2025-06-24-v4/E9AD0E7DCC2B/", "Exp_2025-06-24-v4/E9AD0E7DCC2B/"]

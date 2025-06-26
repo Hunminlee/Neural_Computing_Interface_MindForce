5d0150f68e4d58b7a199fec4f12abf444b83268f
hml76 <hml763@naver.com>
2025-06-26 16:11:25 -0500

    updated results for 6/26-Hunmin data

--- V1_modular/config.py
+++ V1_modular/config.py
@@ -1,12 +1,14 @@
 # Hyperparameter Setting
 
-default_path = "C:/Users/hml76/OneDrive/문서/MATLAB/data/"
+default_path = "C:/Users/hml76/OneDrive/문서/MATLAB/Data_Hunmin/"
+
 classes = ['1/','2/','3/','4/','5/']
 
 Info = ['Stand (5/27)', 'Stand (6/18)', 'Stand (6/20)', 'Sit_chair (6/20)', 'Sit_chair_leg_crossed (6/20)', 'Sit_floor (6/20)',
         'Stand (6/20-v2)', 'Sit_chair (6/20-v2)', 'Sit_chair_leg_crossed (6/20-v2)', 'Sit_floor (6/20-v2)',
         'Stand (6/23)', 'Sit_chair (6/23)', 'Sit_chair_leg_crossed (6/23)', 'Sit_floor (6/23)',
-        'Stand (6/24)', 'Sit_chair (6/24)', 'Sit_chair_leg_crossed (6/24)', 'Sit_floor (6/24)']
+        'Stand (6/24)', 'Sit_chair (6/24)', 'Sit_chair_leg_crossed (6/24)', 'Sit_floor (6/24)',
+        'Stand (6/26)', 'Sit_chair (6/26)', 'Sit_chair_leg_crossed (6/26)', 'Sit_floor (6/26)']
 
 #Date / Bluetooth address
 dataset = ["Exp_2025-05-27/E8331D05289A/", "Exp_2025-06-18/E9AD0E7DCC2B/",

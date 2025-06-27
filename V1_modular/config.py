bf573c67c9479e7b11c8f281495502ff00d58a41
hml76 <hml763@naver.com>
2025-06-27 13:53:36 -0500

    Added daily result

--- V1_modular/config.py
+++ V1_modular/config.py
@@ -8,7 +8,8 @@ Info = ['Stand (5/27)', 'Stand (6/18)', 'Stand (6/20)', 'Sit_chair (6/20)', 'Sit
         'Stand (6/20-v2)', 'Sit_chair (6/20-v2)', 'Sit_chair_leg_crossed (6/20-v2)', 'Sit_floor (6/20-v2)',
         'Stand (6/23)', 'Sit_chair (6/23)', 'Sit_chair_leg_crossed (6/23)', 'Sit_floor (6/23)',
         'Stand (6/24)', 'Sit_chair (6/24)', 'Sit_chair_leg_crossed (6/24)', 'Sit_floor (6/24)',
-        'Stand (6/26)', 'Sit_chair (6/26)', 'Sit_chair_leg_crossed (6/26)', 'Sit_floor (6/26)', ]
+        'Stand (6/26)', 'Sit_chair (6/26)', 'Sit_chair_leg_crossed (6/26)', 'Sit_floor (6/26)',
+        'Stand (6/27)', 'Sit_chair (6/27)', 'Sit_chair_leg_crossed (6/27)', 'Sit_floor (6/27)']
 
 #Date / Bluetooth address
 dataset = ["Exp_2025-05-27/E8331D05289A/", "Exp_2025-06-18/E9AD0E7DCC2B/",
@@ -16,6 +17,7 @@ dataset = ["Exp_2025-05-27/E8331D05289A/", "Exp_2025-06-18/E9AD0E7DCC2B/",
            "Exp_2025-06-20-v5/E9AD0E7DCC2B/", "Exp_2025-06-20-v6/E9AD0E7DCC2B/", "Exp_2025-06-20-v7/E9AD0E7DCC2B/", "Exp_2025-06-20-v8/E9AD0E7DCC2B/",
            "Exp_2025-06-23-v1/E9AD0E7DCC2B/", "Exp_2025-06-23-v2/E9AD0E7DCC2B/", "Exp_2025-06-23-v3/E9AD0E7DCC2B/", "Exp_2025-06-23-v4/E9AD0E7DCC2B/",
            "Exp_2025-06-24-v1/E9AD0E7DCC2B/", "Exp_2025-06-24-v2/E9AD0E7DCC2B/", "Exp_2025-06-24-v3/E9AD0E7DCC2B/", "Exp_2025-06-24-v4/E9AD0E7DCC2B/",
-           "Exp_2025-06-26-v1/E9AD0E7DCC2B/", "Exp_2025-06-26-v2/E9AD0E7DCC2B/", "Exp_2025-06-26-v3/E9AD0E7DCC2B/", "Exp_2025-06-26-v4/E9AD0E7DCC2B/"]
+           "Exp_2025-06-26-v1/E9AD0E7DCC2B/", "Exp_2025-06-26-v2/E9AD0E7DCC2B/", "Exp_2025-06-26-v3/E9AD0E7DCC2B/", "Exp_2025-06-26-v4/E9AD0E7DCC2B/",
+           "Exp_2025-06-27-v1/E9AD0E7DCC2B/", "Exp_2025-06-27-v2/E9AD0E7DCC2B/", "Exp_2025-06-27-v3/E9AD0E7DCC2B/", "Exp_2025-06-27-v4/E9AD0E7DCC2B/"]
 
 

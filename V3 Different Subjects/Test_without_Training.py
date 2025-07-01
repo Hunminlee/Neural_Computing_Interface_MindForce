761ce29b5ae0c7ed2cb70cbdf3d876281e550ea5
hml76 <hml763@naver.com>
2025-07-01 14:40:53 -0500

    inter-subject

--- V3 Different Subjects/Test_without_Training.py
+++ V3 Different Subjects/Test_without_Training.py
@@ -62,7 +62,7 @@ class TremorModelTrainer:
         return float(np.max(history.history['val_accuracy']) * 100)
 
 
-    def train_multiple_dataset(self, path, X_train, y_train, X_test, y_test):
+    def train_multiple_dataset(self, X_train, y_train, X_test, y_test):
         self.model = Model.Original_model_V1(X_train.shape[1:])
 
         history, self.model = Model.Train_model(
@@ -73,7 +73,7 @@ class TremorModelTrainer:
 
         acc = self.model.evaluate(X_test, y_test, verbose=0)[1]
         print(f"Accuracy of test dataset using model V0: {acc * 100:.4f}%")
-        return float(np.max(history.history['val_accuracy']) * 100)
+        return float(np.max(history.history['val_accuracy']) * 100), self.model
 
 
     def test(self, path):
@@ -111,7 +111,7 @@ class TremorModelTrainer:
                 print(X_train_stacked.shape, y_train_stacked.shape), X_test_stacked.shape, y_test_stacked.shape
 
             elif idx == K:
-                acc = self.train_multiple_dataset(path, X_train_stacked, y_train_stacked, X_test_stacked, y_test_stacked)
+                acc, _ = self.train_multiple_dataset(X_train_stacked, y_train_stacked, X_test_stacked, y_test_stacked)
 
             else:
                 acc = self.test(path)
@@ -141,3 +141,40 @@ class TremorModelTrainer:
         df.to_csv(filepath, index=False)
         print(f"Results saved to {filepath}\n\n")
         return df
+
+
+    def return_K_data(self, K):
+        X_train_all, y_train_all = [], []
+        X_test_all, y_test_all = [], []
+
+        for idx, session_info in enumerate(self.dataset_info):
+            print(f"Dataset {idx + 1}/{len(self.dataset_info)} - Session {session_info}\n{'='*40}")
+            path = os.path.join(self.default_path, f'{session_info}/raw/')
+
+            if idx < K:
+                feature_set, labels = utils.get_dataset(path, self.classes, show_labels=False)
+                X_train, y_train, X_test, y_test = utils.split_data(feature_set, labels, ratio=0.99)
+
+                # Stack cumulatively
+                X_train_all.append(X_train)
+                y_train_all.append(y_train)
+
+                # Concatenate all so far
+                X_train_stacked = np.concatenate(X_train_all, axis=0)
+                y_train_stacked = np.concatenate(y_train_all, axis=0)
+
+            else:
+                feature_set, labels = utils.get_dataset(path, self.classes, show_labels=False)
+                _, _, X_test, y_test = utils.split_data(feature_set, labels, ratio=0)
+
+                # Stack cumulatively
+                X_test_all.append(X_test)
+                y_test_all.append(y_test)
+
+                # Concatenate all so far
+                X_test_stacked = np.concatenate(X_test_all, axis=0)
+                y_test_stacked = np.concatenate(y_test_all, axis=0)
+
+        print(X_train_stacked.shape, y_train_stacked.shape), X_test_stacked.shape, y_test_stacked.shape
+
+        return X_train_stacked, y_train_stacked, X_test_stacked, y_test_stacked

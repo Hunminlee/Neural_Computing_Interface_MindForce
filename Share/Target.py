02f860d946f113ebd514396d59d4176b199ebdf2
hml76 <hml763@naver.com>
2025-07-08 15:26:33 -0500

    New stage

---
+++ Share/Target.py
@@ -0,0 +1,120 @@
+import scipy.io
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import importlib
+import warnings
+
+warnings.filterwarnings('ignore')
+from tensorflow.keras.models import load_model
+
+import Model
+#importlib.reload(Model)
+import utils
+import config
+
+
+#Baseline : Training from scratch = Training in one data => This is our target performance
+
+class ProgressiveTrainer:
+    def __init__(self, config, subject):
+        if subject == "Hunmin":
+            self.default_path = config.default_path_sub_H
+            self.info_labels = config.Info_sub_H
+            self.dataset_info = config.dataset_sub_H
+        elif subject == "Xianyu":
+            self.default_path = config.default_path_sub_X
+            self.info_labels = config.Info_sub_X
+            self.dataset_info = config.dataset_sub_X
+        elif subject == "Brian":
+            self.default_path = config.default_path_sub_B
+            self.info_labels = config.Info_sub_B
+            self.dataset_info = config.dataset_sub_B
+        elif subject == "Carlson":
+            self.default_path = config.default_path_sub_C
+            self.info_labels = config.Info_sub_C
+            self.dataset_info = config.dataset_sub_C
+        else:
+            print("subject must be Hunmin, Xianyu, Brian, Carlson")
+            return
+
+        self.classes = config.classes_5
+        self.results = []
+        self.model = None
+        self.batch_size = config.batch_size
+        self.epochs = config.epochs
+
+        self.train_ratio = 0.5
+        self.model_name = "Training_from_scratch"
+
+        self.trained_acc_all = []
+        self.prev_acc_all = []
+        self.init_acc_all = []
+        self.X_test_prev_all = []
+        self.y_test_prev_all = []
+
+    def get_result(self, path, first_time, plot_learning_curve):
+        feature_set, labels = utils.get_dataset(path, self.classes, show_labels=False)
+        X_train, y_train, X_test, y_test = utils.split_data(feature_set, labels, ratio=self.train_ratio)
+
+        model = Model.Original_model_V1(X_train.shape[1:])
+        init_acc = model.evaluate(X_test, y_test, verbose=0)[1]
+
+        history, _ = Model.Train_model(
+            model, X_train, y_train, X_test, y_test,
+            self.epochs, self.batch_size, self.model_name,
+            set_verbose=0, save_model_set=False
+        )
+
+        if plot_learning_curve:
+            utils.visualize_history(history)
+
+        test_acc = float(np.max(history.history['val_accuracy']))
+
+        if first_time:
+            self.X_test_prev_all = X_test
+            self.y_test_prev_all = y_test
+        else:
+            self.X_test_prev_all = np.concatenate((self.X_test_prev_all, X_test), axis=0)
+            self.y_test_prev_all = np.concatenate((self.y_test_prev_all, y_test), axis=0)
+
+        prev_acc = model.evaluate(self.X_test_prev_all, self.y_test_prev_all, verbose=0)[1]
+
+        return init_acc, test_acc, prev_acc
+
+    def run(self, plot_learning_curve=False):
+        for idx, session in enumerate(self.dataset_info):
+            print(f"\n{'='*43}\nDataset {idx+1}/{len(self.dataset_info)} - Session {session}\n{'='*43}")
+            path = os.path.join(self.default_path, session + 'raw/')
+            first_time = (idx == 0)
+            init_acc, test_acc, prev_acc = self.get_result(path, first_time, plot_learning_curve)
+
+            self.init_acc_all.append(init_acc)
+            self.trained_acc_all.append(test_acc)
+            self.prev_acc_all.append(prev_acc)
+
+        return self.init_acc_all, self.trained_acc_all, self.prev_acc_all
+
+
+    def plot_results(self, baselines, baseline_K):
+
+        plt.figure(figsize=(15, 8))
+        plt.title('Training from scratch every time', fontsize=15)
+        #plt.plot(self.info_labels, self.init_acc_all, marker='o', label='Untrained model on current data', linestyle='--')
+        plt.plot(self.info_labels, [1/6 for i in range(len(self.info_labels))], marker='o', label='Untrained model on current data', linestyle='--')
+        plt.plot(self.info_labels, self.prev_acc_all, marker='o', label='Model trained + tested on accumulated data')
+        plt.plot(self.info_labels, self.trained_acc_all, marker='o', label='Model trained + tested on current split')
+
+        for idx, base in enumerate(baselines):
+            baseline_result = pd.read_csv(base)
+            plt.plot(self.info_labels, baseline_result['Accuracy'] / 100, marker='^', label=f'Baseline V{idx} - K:{baseline_K[idx]}', linestyle='--')
+
+        plt.ylim([0, 1])
+        plt.xlabel('Date (Sessions)')
+        plt.ylabel('Test Accuracy')
+        plt.xticks(rotation=60)
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
---
+++ Share/config.py
@@ -0,0 +1,49 @@
+# Hyperparameter Setting
+
+classes_5 = ['1/','2/','3/','4/','5/']
+classes_7 = ['1/','2/','3/','4/','5/','6/','7/']
+epochs, epochs_CL = 50, 20
+batch_size = 256
+
+default_path_sub_H = "C:/Users/hml76/OneDrive/문서/MATLAB/Data_Hunmin/"
+default_path_sub_X = "C:/Users/hml76/OneDrive/문서/MATLAB/Data_Xianyu/"
+default_path_sub_B = "C:/Users/hml76/OneDrive/문서/MATLAB/Data_Brian/"
+default_path_sub_C = "C:/Users/hml76/OneDrive/문서/MATLAB/Data_Carlson/"
+
+Info_sub_H = ['Stand (5/27)', 'Stand (6/18)', 'Stand (6/20)', 'Sit_chair (6/20)', 'Sit_chair_leg_crossed (6/20)', 'Sit_floor (6/20)',
+            'Stand (6/20-v2)', 'Sit_chair (6/20-v2)', 'Sit_chair_leg_crossed (6/20-v2)', 'Sit_floor (6/20-v2)',
+            'Stand (6/23)', 'Sit_chair (6/23)', 'Sit_chair_leg_crossed (6/23)', 'Sit_floor (6/23)',
+            'Stand (6/24)', 'Sit_chair (6/24)', 'Sit_chair_leg_crossed (6/24)', 'Sit_floor (6/24)',
+            'Stand (6/26)', 'Sit_chair (6/26)', 'Sit_chair_leg_crossed (6/26)', 'Sit_floor (6/26)',
+            'Stand (6/27)', 'Sit_chair (6/27)', 'Sit_chair_leg_crossed (6/27)', 'Sit_floor (6/27)',
+            'Stand (6/30)', 'Sit_chair (6/30)', 'Sit_chair_leg_crossed (6/30)', 'Sit_floor (6/30)',
+            'Stand (7/1)', 'Sit_chair (7/1)', 'Sit_chair_leg_crossed (7/1)', 'Sit_floor (7/1)',
+            'Stand (7/2)', 'Sit_chair (7/2)', 'Sit_chair_leg_crossed (7/2)', 'Sit_floor (7/2)',
+            'Stand (7/3)', 'Sit_chair (7/3)', 'Sit_chair_leg_crossed (7/3)', 'Sit_floor (7/3)',
+            'Stand (7/7)', 'Sit_chair (7/7)', 'Sit_chair_leg_crossed (7/7)', 'Sit_floor (7/7)']
+
+Info_sub_X = ['Stand (6/24)', 'Sit_chair (6/24)', 'Stand (6/26)', 'Sit_chair (6/26)', 'Stand (6/27)', 'Sit_chair (6/27)', 'Stand (6/30)', 'Sit_chair (6/30)',
+              'Stand (7/1)', 'Sit_chair (7/1)', 'Stand (7/2)', 'Sit_chair (7/2)']
+Info_sub_B = ['Stand (6/27)', 'Sit_chair (6/27)']
+Info_sub_C = ['Stand (6/30)', 'Sit_chair (6/30)']
+
+#Date / Bluetooth address
+dataset_sub_H = ["Exp_2025-05-27/E8331D05289A/", "Exp_2025-06-18/E9AD0E7DCC2B/",
+            "Exp_2025-06-20-v1/E9AD0E7DCC2B/", "Exp_2025-06-20-v2/E9AD0E7DCC2B/", "Exp_2025-06-20-v3/E9AD0E7DCC2B/", "Exp_2025-06-20-v4/E9AD0E7DCC2B/",
+            "Exp_2025-06-20-v5/E9AD0E7DCC2B/", "Exp_2025-06-20-v6/E9AD0E7DCC2B/", "Exp_2025-06-20-v7/E9AD0E7DCC2B/", "Exp_2025-06-20-v8/E9AD0E7DCC2B/",
+            "Exp_2025-06-23-v1/E9AD0E7DCC2B/", "Exp_2025-06-23-v2/E9AD0E7DCC2B/", "Exp_2025-06-23-v3/E9AD0E7DCC2B/", "Exp_2025-06-23-v4/E9AD0E7DCC2B/",
+            "Exp_2025-06-24-v1/E9AD0E7DCC2B/", "Exp_2025-06-24-v2/E9AD0E7DCC2B/", "Exp_2025-06-24-v3/E9AD0E7DCC2B/", "Exp_2025-06-24-v4/E9AD0E7DCC2B/",
+            "Exp_2025-06-26-v1/E9AD0E7DCC2B/", "Exp_2025-06-26-v2/E9AD0E7DCC2B/", "Exp_2025-06-26-v3/E9AD0E7DCC2B/", "Exp_2025-06-26-v4/E9AD0E7DCC2B/",
+            "Exp_2025-06-27-v1/E9AD0E7DCC2B/", "Exp_2025-06-27-v2/E9AD0E7DCC2B/", "Exp_2025-06-27-v3/E9AD0E7DCC2B/", "Exp_2025-06-27-v4/E9AD0E7DCC2B/",
+            "Exp_2025-06-30-v1/E9AD0E7DCC2B/", "Exp_2025-06-30-v2/E9AD0E7DCC2B/", "Exp_2025-06-30-v3/E9AD0E7DCC2B/", "Exp_2025-06-30-v4/E9AD0E7DCC2B/",
+            "Exp_2025-07-01-v1/E9AD0E7DCC2B/", "Exp_2025-07-01-v2/E9AD0E7DCC2B/", "Exp_2025-07-01-v3/E9AD0E7DCC2B/", "Exp_2025-07-01-v4/E9AD0E7DCC2B/",
+            "Exp_2025-07-02-v1/E9AD0E7DCC2B/", "Exp_2025-07-02-v2/E9AD0E7DCC2B/", "Exp_2025-07-02-v3/E9AD0E7DCC2B/", "Exp_2025-07-02-v4/E9AD0E7DCC2B/",
+            "Exp_2025-07-03-v1/E9AD0E7DCC2B/", "Exp_2025-07-03-v2/E9AD0E7DCC2B/", "Exp_2025-07-03-v3/E9AD0E7DCC2B/", "Exp_2025-07-03-v4/E9AD0E7DCC2B/",
+            "Exp_2025-07-07-v1/E9AD0E7DCC2B/", "Exp_2025-07-07-v2/E9AD0E7DCC2B/", "Exp_2025-07-07-v3/E9AD0E7DCC2B/", "Exp_2025-07-07-v4/E9AD0E7DCC2B/"]
+
+dataset_sub_X = ["Exp_2025-06-24-v1/E9AD0E7DCC2B/", "Exp_2025-06-24-v2/E9AD0E7DCC2B/", "Exp_2025-06-26-v1/E9AD0E7DCC2B/", "Exp_2025-06-26-v2/E9AD0E7DCC2B/",
+                 "Exp_2025-06-27-v1/E9AD0E7DCC2B/", "Exp_2025-06-27-v2/E9AD0E7DCC2B/", "Exp_2025-06-30-v1/FEFFF6FFF5FF/", "Exp_2025-06-30-v2/FEFFF6FFF5FF/",
+                 "Exp_2025-07-01-v1/E9AD0E7DCC2B/", "Exp_2025-07-01-v2/E9AD0E7DCC2B/", "Exp_2025-07-02-v1/E9AD0E7DCC2B/", "Exp_2025-07-02-v2/E9AD0E7DCC2B/"]
+
+dataset_sub_B = ["Exp_2025-06-27-v1/E9AD0E7DCC2B/", "Exp_2025-06-27-v2/E9AD0E7DCC2B/"]
+dataset_sub_C = ["Exp_2025-06-30-v1/E9AD0E7DCC2B/", "Exp_2025-06-30-v2/E9AD0E7DCC2B/"]

a86bbaab11d58fb43993183aa519c2ff94b35b55
hml76 <hml763@naver.com>
2025-07-11 01:27:41 -0500

    added daily dataset

--- Share/config.py
+++ Share/config.py
@@ -22,13 +22,14 @@ Info_sub_H = ['Stand (5/27)', 'Stand (6/18)', 'Stand (6/20)', 'Sit_chair (6/20)'
             'Stand (7/2)', 'Sit_chair (7/2)', 'Sit_chair_leg_crossed (7/2)', 'Sit_floor (7/2)',
             'Stand (7/3)', 'Sit_chair (7/3)', 'Sit_chair_leg_crossed (7/3)', 'Sit_floor (7/3)',
             'Stand (7/7)', 'Sit_chair (7/7)', 'Sit_chair_leg_crossed (7/7)', 'Sit_floor (7/7)',
-            'Stand (7/9)', 'Sit_chair (7/9)', 'Sit_chair_leg_crossed (7/9)', 'Sit_floor (7/9)', ]
+            'Stand (7/9)', 'Sit_chair (7/9)', 'Sit_chair_leg_crossed (7/9)', 'Sit_floor (7/9)',
+            'Stand (7/10)', 'Sit_chair (7/10)', 'Sit_chair_leg_crossed (7/10)', 'Sit_floor (7/10)']
 
 Info_sub_X = ['Stand (6/24)', 'Sit_chair (6/24)', 'Stand (6/26)', 'Sit_chair (6/26)', 'Stand (6/27)', 'Sit_chair (6/27)', 'Stand (6/30)', 'Sit_chair (6/30)',
-              'Stand (7/1)', 'Sit_chair (7/1)', 'Stand (7/2)', 'Sit_chair (7/2)', 'Stand (7/9)', 'Sit_chair (7/9)']
-Info_sub_B = ['Stand (6/27)', 'Sit_chair (6/27)', 'Stand (7/9)', 'Sit_chair (7/9)']
-Info_sub_C = ['Stand (6/30)', 'Sit_chair (6/30)', 'Stand (7/9)', 'Sit_chair (7/9)']
-Info_sub_H2 = ['Stand (7/9)', 'Sit_chair (7/9)']
+              'Stand (7/1)', 'Sit_chair (7/1)', 'Stand (7/2)', 'Sit_chair (7/2)', 'Stand (7/9)', 'Sit_chair (7/9)', 'Stand (7/10)', 'Sit_chair (7/10)']
+Info_sub_B = ['Stand (6/27)', 'Sit_chair (6/27)', 'Stand (7/9)', 'Sit_chair (7/9)', 'Stand (7/10)', 'Sit_chair (7/10)']
+Info_sub_C = ['Stand (6/30)', 'Sit_chair (6/30)', 'Stand (7/9)', 'Sit_chair (7/9)', 'Stand (7/10)', 'Sit_chair (7/10)']
+Info_sub_H2 = ['Stand (7/9)', 'Sit_chair (7/9)', 'Stand (7/10)', 'Sit_chair (7/10)']
 
 
 #Date / Bluetooth address
@@ -44,13 +45,16 @@ dataset_sub_H = ["Exp_2025-05-27/E8331D05289A/", "Exp_2025-06-18/E9AD0E7DCC2B/",
             "Exp_2025-07-02-v1/E9AD0E7DCC2B/", "Exp_2025-07-02-v2/E9AD0E7DCC2B/", "Exp_2025-07-02-v3/E9AD0E7DCC2B/", "Exp_2025-07-02-v4/E9AD0E7DCC2B/",
             "Exp_2025-07-03-v1/E9AD0E7DCC2B/", "Exp_2025-07-03-v2/E9AD0E7DCC2B/", "Exp_2025-07-03-v3/E9AD0E7DCC2B/", "Exp_2025-07-03-v4/E9AD0E7DCC2B/",
             "Exp_2025-07-07-v1/E9AD0E7DCC2B/", "Exp_2025-07-07-v2/E9AD0E7DCC2B/", "Exp_2025-07-07-v3/E9AD0E7DCC2B/", "Exp_2025-07-07-v4/E9AD0E7DCC2B/",
-            "Exp_2025-07-09-v1/E9AD0E7DCC2B/", "Exp_2025-07-09-v2/E9AD0E7DCC2B/", "Exp_2025-07-09-v3/E9AD0E7DCC2B/", "Exp_2025-07-09-v4/E9AD0E7DCC2B/"]
+            "Exp_2025-07-09-v1/E9AD0E7DCC2B/", "Exp_2025-07-09-v2/E9AD0E7DCC2B/", "Exp_2025-07-09-v3/E9AD0E7DCC2B/", "Exp_2025-07-09-v4/E9AD0E7DCC2B/",
+            "Exp_2025-07-10-v1/E9AD0E7DCC2B/", "Exp_2025-07-10-v2/E9AD0E7DCC2B/", "Exp_2025-07-10-v3/E9AD0E7DCC2B/", "Exp_2025-07-10-v4/E9AD0E7DCC2B/"]
 
 dataset_sub_X = ["Exp_2025-06-24-v1/E9AD0E7DCC2B/", "Exp_2025-06-24-v2/E9AD0E7DCC2B/", "Exp_2025-06-26-v1/E9AD0E7DCC2B/", "Exp_2025-06-26-v2/E9AD0E7DCC2B/",
                  "Exp_2025-06-27-v1/E9AD0E7DCC2B/", "Exp_2025-06-27-v2/E9AD0E7DCC2B/", "Exp_2025-06-30-v1/FEFFF6FFF5FF/", "Exp_2025-06-30-v2/FEFFF6FFF5FF/",
                  "Exp_2025-07-01-v1/E9AD0E7DCC2B/", "Exp_2025-07-01-v2/E9AD0E7DCC2B/", "Exp_2025-07-02-v1/E9AD0E7DCC2B/", "Exp_2025-07-02-v2/E9AD0E7DCC2B/",
-                 "Exp_2025-07-09-v1/E9AD0E7DCC2B/", "Exp_2025-07-09-v2/E9AD0E7DCC2B/"]
+                 "Exp_2025-07-09-v1/E9AD0E7DCC2B/", "Exp_2025-07-09-v2/E9AD0E7DCC2B/", "Exp_2025-07-10-v1/E9AD0E7DCC2B/", "Exp_2025-07-10-v2/E9AD0E7DCC2B/"]
 
-dataset_sub_B = ["Exp_2025-06-27-v1/E9AD0E7DCC2B/", "Exp_2025-06-27-v2/E9AD0E7DCC2B/", "Exp_2025-07-09-v1/E9AD0E7DCC2B/", "Exp_2025-07-09-v2/E9AD0E7DCC2B/"]
-dataset_sub_C = ["Exp_2025-06-30-v1/E9AD0E7DCC2B/", "Exp_2025-06-30-v2/E9AD0E7DCC2B/", "Exp_2025-07-09-v1/E9AD0E7DCC2B/", "Exp_2025-07-09-v2/E9AD0E7DCC2B/"]
-dataset_sub_H2 = ["Exp_2025-07-09-v1/E9AD0E7DCC2B/", "Exp_2025-07-09-v2/E9AD0E7DCC2B/"]
+dataset_sub_B = ["Exp_2025-06-27-v1/E9AD0E7DCC2B/", "Exp_2025-06-27-v2/E9AD0E7DCC2B/", "Exp_2025-07-09-v1/E9AD0E7DCC2B/", "Exp_2025-07-09-v2/E9AD0E7DCC2B/",
+                 "Exp_2025-07-10-v1/E9AD0E7DCC2B/", "Exp_2025-07-10-v2/E9AD0E7DCC2B/"]
+dataset_sub_C = ["Exp_2025-06-30-v1/E9AD0E7DCC2B/", "Exp_2025-06-30-v2/E9AD0E7DCC2B/", "Exp_2025-07-09-v1/E9AD0E7DCC2B/", "Exp_2025-07-09-v2/E9AD0E7DCC2B/",
+                 "Exp_2025-07-10-v1/E9AD0E7DCC2B/", "Exp_2025-07-10-v2/E9AD0E7DCC2B/"]
+dataset_sub_H2 = ["Exp_2025-07-09-v1/E9AD0E7DCC2B/", "Exp_2025-07-09-v2/E9AD0E7DCC2B/", "Exp_2025-07-10-v1/E9AD0E7DCC2B/", "Exp_2025-07-10-v2/E9AD0E7DCC2B/"]
